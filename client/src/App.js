e8eeca4edc1673a3a5eb115f3c2cc9d0154cf46a
wmujahid <warandpeace97@gmail.com>
2023-11-29 01:48:28 -0500

    mvp login and auth for users

---
+++ client/src/App.js
@@ -0,0 +1,73 @@
+import React, { Fragment, useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate, Link } from "react-router-dom";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import Register from "./components/Register.js";
+
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const checkAuthenticated = async () => {
+    try {
+      const res = await fetch("http://localhost:5000/authentication/verify", {
+        method: "POST",
+        headers: { jwt_token: localStorage.token }
+      });
+
+      const parseRes = await res.json();
+
+      parseRes === true ? setIsAuthenticated(true) : setIsAuthenticated(false);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    checkAuthenticated();
+  }, []);
+
+  const setAuth = boolean => {
+    setIsAuthenticated(boolean);
+  };
+
+  return (
+    <Fragment>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <div>
+                  <h1>Welcome to the App!</h1>
+                  <p>Please register or login to continue.</p>
+                  <Link to="/register">Register</Link>
+                  <br />
+                  <Link to="/login">Login</Link>
+                </div>
+              }
+            />
+            <Route
+              exact
+              path="/login"
+              element={!isAuthenticated ? <Login setAuth={setAuth} /> : <Navigate to="/dashboard" />}
+            />
+            <Route
+              exact
+              path="/register"
+              element={!isAuthenticated ? <Register setAuth={setAuth} /> : <Navigate to="/dashboard" />}
+            />
+            <Route
+              exact
+              path="/dashboard"
+              element={isAuthenticated ? <Dashboard setAuth={setAuth} /> : <Navigate to="/login" />}
+            />
+          </Routes>
+        </div>
+      </Router>
+    </Fragment>
+  );
+}
+
+export default App;
--- server/database.sql
+++ server/database.sql
@@ -1,44 +1,24 @@
 CREATE DATABASE playlistpal;
 
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  username VARCHAR(255) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  email VARCHAR(255) UNIQUE NOT NULL,
+--CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users(
+  user_id uuid DEFAULT uuid_generate_v4(),
+  user_name VARCHAR(255) NOT NULL,
+  user_email VARCHAR(255) NOT NULL UNIQUE,
+  user_password VARCHAR(255) NOT NULL,
+  PRIMARY KEY(user_id),
   created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE playlists (
-  id SERIAL PRIMARY KEY,
-  user_id INTEGER REFERENCES users(id) NOT NULL,
-  description VARCHAR(255) NOT NULL,
-  name VARCHAR(255) NOT NULL,
+CREATE TABLE playlists(
+  playlist_id uuid DEFAULT uuid_generate_v4(),
+  playlist_name VARCHAR(255) NOT NULL,
+  playlist_description VARCHAR(255) NOT NULL,
+  playlist_image VARCHAR(255) NOT NULL,
+  playlist_url VARCHAR(255) NOT NULL,
+  playlist_owner uuid NOT NULL,
+  PRIMARY KEY(playlist_id),
+  FOREIGN KEY(playlist_owner) REFERENCES users(user_id) ON DELETE CASCADE,
   created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
-
-CREATE TABLE songs (
-  id SERIAL PRIMARY KEY,
-  playlist_id INTEGER REFERENCES playlists(id) NOT NULL,
-  title VARCHAR(255) NOT NULL,
-  artist VARCHAR(255) NOT NULL,
-  album VARCHAR(255) NOT NULL,
-  genre VARCHAR(255) NOT NULL,
-  rating INTEGER NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT NOW()
-);
-
-CREATE TABLE collaborators (
-  playlist_id INTEGER REFERENCES playlists(id) NOT NULL,
-  user_id INTEGER REFERENCES users(id) NOT NULL,
-  can_edit BOOLEAN NOT NULL
-);
-
-CREATE TABLE playlist_analytics (
-  playlist_id INTEGER REFERENCES playlists(id) NOT NULL,
-  plays_count INTEGER NOT NULL,
-  likes_count INTEGER NOT NULL,
-  songs_added INTEGER NOT NULL,
-  songs_removed INTEGER NOT NULL,
-  last_updated TIMESTAMP NOT NULL DEFAULT NOW()
-);
-  
